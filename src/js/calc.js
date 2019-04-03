9e74cce3168035ba517fe9c72b7d9470ac8e6073
Szymon Jeż <jezyk2796@op.pl>
2019-04-03 21:47:44 +0200

    calculator works

---
+++ src/js/calc.js
@@ -0,0 +1,50 @@
+const output = document.querySelector("#output-value");
+const historyValue = document.querySelector('#history-value');
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const calculate = () => {
+    output.innerText = eval(output.innerText);
+};
+
+const showHistory = (result) => {
+    historyValue.innerText = result;
+};
+
+const clear = () => {
+    output.innerText = "";
+};
+
+const backspace = () => {
+    let value = output.innerText;
+    value = value.substr(0, value.length - 1);
+    output.innerText = value;
+};
+
+const insertValue = e => {
+    if (
+        e.target.id != "clear" &&
+        e.target.id != "backspace" &&
+        e.target.id != "equals"
+    ) {
+        output.innerText += e.target.innerText;
+    } else if (e.target.id == "clear") {
+        clear();
+    } else if (e.target.id == "backspace") {
+        backspace();
+    }
+
+    if (e.target.id == "equals") {
+        const historyResult = `${output.innerText} =`;
+
+        calculate();
+        showHistory(historyResult);
+    }
+};
+
+numbers.forEach(number => number.addEventListener("click", insertValue));
+operators.forEach(operator => operator.addEventListener("click", insertValue));
+
+export {
+    insertValue
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
 import './../style/main.scss';
+
+import "./calc.js";
